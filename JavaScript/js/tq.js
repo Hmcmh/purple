32aa6f41668ad8f09fe2b3a5bdf42189c880b75d
Hmcmh <2480821407@qq.com>
2019-08-09 18:01:50 +0800

    tijiao

---
+++ JavaScript/js/tq.js
@@ -0,0 +1,14 @@
+window.onload = function(){
+
+	var oIt = document.getElementById('hq');
+	var oHh = document.getElementById('hh');
+	var lI = document.getElementById('lll')
+
+
+	oIt.onclick = function(){
+		lll.href = './css/sk1.css';
+	}
+	oHh.onclick = function(){
+		lll.href = './css/sk2.css';
+	}
+}
---
+++ JavaScript/onload.js
@@ -0,0 +1,6 @@
+window.onload需要页面上所有的资源(图片,音频,视频等)被加载以后才会触发
+
+window.onload = function(){
+
+
+}
